f96a8368bff3a6f37bbe127fb2012c2faf2137be
Douglas Wilhelm <dwilhelm@vacayhomeconnect.com>
2019-07-09 20:29:18 -0500

    Correct issue with httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+  ],
+  exports: [
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
